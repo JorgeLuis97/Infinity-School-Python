54d827ea1662a4ebc42926ec02c362b0861ed1f2
Jorge Luis Gomes Rabelo <110363635+JorgeLuis97@users.noreply.github.com>
2023-08-31 21:31:07 -0300

    Trans. projeto

--- CRUD/consulta.py
+++
@@ -1,9 +0,0 @@
-from banco import obterconta
-
-
-def consultarsaldo(conta: int) -> None:
-    cliente = obterconta(conta)
-    if cliente:
-        print(f"Saldo: R$ {cliente['saldo']}")
-    else:
-        print("Cliente não existe!")
